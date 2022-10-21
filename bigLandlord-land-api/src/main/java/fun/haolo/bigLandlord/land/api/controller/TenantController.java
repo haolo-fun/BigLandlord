35d3a3052ccc76dab3105cb990885b7c5dba05a7
haolo <50327814+haolo-fun@users.noreply.github.com>
2022-10-21 19:33:07 +0800

    add documentation comments

--- bigLandlord-land-api/src/main/java/fun/haolo/bigLandlord/land/api/controller/TenantController.java
+++ bigLandlord-land-api/src/main/java/fun/haolo/bigLandlord/land/api/controller/TenantController.java
@@ -4,6 +4,8 @@ import fun.haolo.bigLandlord.core.api.ResponseResult;
 import fun.haolo.bigLandlord.db.entity.Tenant;
 import fun.haolo.bigLandlord.db.param.TenantParam;
 import fun.haolo.bigLandlord.db.service.ITenantService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,12 +23,14 @@ import java.util.ArrayList;
  */
 @RestController
 @RequestMapping("/tenant")
+@Api(tags = "land_租户控制接口")
 public class TenantController {
 
     @Autowired
     ITenantService tenantService;
 
     @PostMapping("/add")
+    @ApiOperation(value = "添加租户")
     public ResponseResult<Tenant> add(TenantParam param) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username = userDetails.getUsername();
@@ -36,6 +40,7 @@ public class TenantController {
     }
 
     @DeleteMapping("/del/{id}")
+    @ApiOperation(value = "删除租户")
     public ResponseResult<Object> delete(@PathVariable Long id) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username = userDetails.getUsername();
@@ -44,6 +49,7 @@ public class TenantController {
     }
 
     @PutMapping("/update")
+    @ApiOperation(value = "更新租户信息")
     public ResponseResult<Tenant> update(TenantParam param) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username = userDetails.getUsername();
@@ -53,6 +59,7 @@ public class TenantController {
     }
 
     @GetMapping("/get/{page}/{count}")
+    @ApiOperation(value = "查询所有租户信息")
     public ResponseResult<ArrayList<TenantParam>> selectAll(@PathVariable Integer page, @PathVariable Integer count) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username = userDetails.getUsername();
@@ -62,6 +69,7 @@ public class TenantController {
     }
 
     @GetMapping({"/get/{name}/{page}/{count}"})
+    @ApiOperation(value = "通过租户姓名查询信息")
     public ResponseResult<ArrayList<TenantParam>> selectAll(@PathVariable String name, @PathVariable Integer page, @PathVariable Integer count) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username = userDetails.getUsername();
